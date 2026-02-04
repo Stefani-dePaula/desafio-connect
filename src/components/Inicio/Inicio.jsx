1a4c1a12ac7ec6b7d3892a80a39a29abe6327118
Stefani-dePaula <stefanidepauladasilva1@hotmail.com>
2026-02-04 19:33:14 -0300

    Finalizamos a navBar

--- src/components/Inicio/Inicio.jsx
+++ src/components/Inicio/Inicio.jsx
@@ -1,10 +1,19 @@
-import React from "react"
+import styles from './Inicio.module.css'
+importImgInicial from "../../assets/img/inicio-img.png"
+
 function Inicio() {
   return (
-    <>
-    
-    <h2> Sou o Inicio </h2>
-    </>
+    <section className={styles.containerTelaInicial}>
+    <div>  
+    <h1> Projetos Sociais que Transformam </h1>
+    <p>Conectando sua empresa com projetos sociais impactantes. Juntos, criamos mudanças reais na comunidade
+
+    </p>
+    </div>
+
+    <img src="" alt="Imagem da tela inicial" />
+
+    </section>
   )
 }
 export default Inicio
